6edd1a02970ae1d8afe2140680165cc3079c34d9
joaofelix27 <joaofelix27@gmail.com>
2022-07-08 14:24:29 -0300

    Corrigindo schema

--- src/schemas/registerSchema.js
+++ src/schemas/registerSchema.js
@@ -1,16 +1,14 @@
 import joi from "joi";
 
 const registerSchema = joi.object({
-    name: joi
-      .string()
-      .pattern(/^[a-zA-Z\s]*$/)
-      .required(), // Only accepts upper or lower case letters or blank spaces
-      
-    email: joi.string().email().required(),
-    cpf: joi
-    .alphanum()
-    .required(),
-    password: joi.string().required(),
-  });
+  name: joi
+    .string()
+    .pattern(/^[a-zA-Z\s]*$/)
+    .required(), // Only accepts upper or lower case letters or blank spaces
+
+  email: joi.string().email().required(),
+  cpf: joi.string().alphanum().required(),
+  password: joi.string().required(),
+});
 
 export default registerSchema;
