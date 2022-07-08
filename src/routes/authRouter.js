a9c4ab7be0208dea917cb33d2ea122cedda6f1a2
joaofelix27 <joaofelix27@gmail.com>
2022-07-08 00:03:28 -0300

    Validando Cadastro e Login

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { registerUser, loginUser } from "../controllers/userController.js";
+import validateLogin from "../middlewares/loginMiddleware.js";
+import validateRegister from "../middlewares/registerMiddleware.js";
 
 
 const authRouter= Router ()
 
-authRouter.post("/cadastro",registerUser);
+authRouter.post("/cadastro", validateRegister, registerUser);
 
-authRouter.post("/login",loginUser);
+authRouter.post("/login",validateLogin, loginUser);
 
 export default authRouter;
