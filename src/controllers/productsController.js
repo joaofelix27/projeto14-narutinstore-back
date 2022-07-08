b7140f9e04ad81ce97361c63ae875948c0d1c733
joaofelix27 <joaofelix27@gmail.com>
2022-07-08 16:34:26 -0300

    Mandando informação do produto para o front

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,12 +1,24 @@
-import db from '../db/db.js';
+import db from "../db/db.js";
 
-export async function productList(req, res){
-    try{
-        const products = await db.collection("inventory").find().toArray();
-        res.status(200).send(products)
+export async function productList(req, res) {
+  try {
+    const products = await db.collection("inventory").find().toArray();
+    res.status(200).send(products);
+  } catch {
+    res.sendStatus(500);
+  }
+}
+export async function chosenProduct(req, res) {
+  const name = req.params.name;
+  console.log(name);
+  try {
+    const products = await db.collection("inventory").find({name}).toArray();
+    if (products.length === 0) {
+      return res.sendStatus(404);
+    } else {
+      res.status(200).send(products);
     }
-
-    catch{
-        res.sendStatus(500);
-    }
+  } catch {
+    res.sendStatus(500);
+  }
 }
