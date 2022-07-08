e1d3c5c1af1e316871d57cbf525129d1c9cfc854
joaofelix27 <joaofelix27@gmail.com>
2022-07-07 23:18:04 -0300

    Validando CPF

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,53 +2,56 @@ import db from "../db/db.js";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
 
-  
 export async function registerUser(req, res) {
-    let { email, name, cpf, password } = req.body;
-  
-    const encryptedPassword = bcrypt.hashSync(password, 10);
-  
-    try {
-      const alreadyExists = await db
-        .collection("users")
-        .find({ email: email })
-        .toArray();
-      if (alreadyExists.length === 0) {
-        await db
-          .collection("users")
-          .insertOne({ ...req.body, password: encryptedPassword });
-        res.sendStatus(201);
-      } else {
-        res.sendStatus(409);
-      }
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
-  }
+  let { email, name, cpf, password } = req.body;
 
-  export   async function loginUser(req, res) {
-    let { email, password } = req.body;
-    try {
-      const loginData = await db.collection("users").findOne({ email: email });
-      const name= loginData.name
-      const verifyPassword = bcrypt.compareSync(password, loginData.password);
-      if (loginData.length !== 0) {
-        if (email == loginData.email && verifyPassword) {
-          const token = uuid();
-          await db.collection("sessions").insertOne({
-            token,
-            userId: loginData._id,
-          });
-          res.status(200).send({ token, name});
-        } else {
-          res.status(401).send("E-mail ou senha inválidos!");
-        }
+  const encryptedPassword = bcrypt.hashSync(password, 10);
+
+  try {
+    const alreadyExists = await db
+      .collection("users")
+      .find({ email: email })
+      .toArray();
+    const cpfExists = await db.collection("users").find({ cpf }).toArray();
+    if (cpfExists.length !== 0) {
+        return res.sendStatus(408);
+    }
+    if (alreadyExists.length === 0) {
+      await db
+        .collection("users")
+        .insertOne({ ...req.body, password: encryptedPassword });
+      res.sendStatus(201);
+    } else {
+      res.sendStatus(409);
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function loginUser(req, res) {
+  let { email, password } = req.body;
+  try {
+    const loginData = await db.collection("users").findOne({ email: email });
+    const name = loginData.name;
+    const verifyPassword = bcrypt.compareSync(password, loginData.password);
+    if (loginData.length !== 0) {
+      if (email == loginData.email && verifyPassword) {
+        const token = uuid();
+        await db.collection("sessions").insertOne({
+          token,
+          userId: loginData._id,
+        });
+        res.status(200).send({ token, name });
       } else {
         res.status(401).send("E-mail ou senha inválidos!");
       }
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
+    } else {
+      res.status(401).send("E-mail ou senha inválidos!");
     }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
   }
+}
