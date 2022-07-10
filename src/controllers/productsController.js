7d7fb378cb9462cff36a33534fe7217933933ac1
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2022-07-10 19:55:13 -0300

    Merge feito com sucesso

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -11,13 +11,8 @@ export async function productList(req, res) {
 export async function chosenProduct(req, res) {
   const name = req.params.name;
   try {
-<<<<<<< HEAD
-    const products = await db.collection("inventory").findOne({name})
-    if (products.length === 0) {
-=======
     const products = await db.collection("inventory").findOne({name});
     if (!products) {
->>>>>>> 32ed221131caac93dbe7e6598ae4c1400a4a54f6
       return res.sendStatus(404);
     } else {
       res.status(200).send(products);
